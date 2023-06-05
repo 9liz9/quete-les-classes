e539d978afb985520c19062b874684162c1e4da9
Liz Manresa <manresa.liz@gmail.com>
2023-06-06 00:01:38 +0200

    quest class done

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿namespace S03_classes;
+
+public class Character
+{
+    public string Name;
+    public int HealthPoints;
+    public int AttackStrength;
+    public int DefenseStrength;
+
+    public bool IsAlive()
+    {
+        return HealthPoints > 0;
+    }
+
+    public void Attack(Character otherCharacter)
+    {
+        int damage = Math.Max(0, AttackStrength - otherCharacter.DefenseStrength);
+        otherCharacter.HealthPoints -= damage;
+        Console.WriteLine($"{Name} attaque {otherCharacter.Name} et inflige {damage} points de perte .");
+    }
+}
+
+class Program
+{
+    
+    static void Main(string[] args)
+    {
+        Character playerOne = new Character
+        {
+            Name = "Player One",
+            HealthPoints = 30,
+            AttackStrength = 5,
+            DefenseStrength = 5
+        };
+
+        Character playerTwo = new Character
+        {
+            Name = "Player Two",
+            HealthPoints = 30,
+            AttackStrength = 6,
+            DefenseStrength = 4
+        };
+
+        while (playerOne.IsAlive() && playerTwo.IsAlive())
+        {
+            playerOne.Attack(playerTwo);
+            if (playerTwo.IsAlive())
+                playerTwo.Attack(playerOne);
+        }
+
+        Character winner; 
+        if (playerOne.IsAlive())
+        winner = playerOne;
+        else
+        winner = playerTwo;
+
+        Console.WriteLine($"{winner.Name} a gagné!");
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# quete-les-classes
